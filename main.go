e6758af1b138f86f121e0ba454dd6f4693d9d93f
SeniorShadifer <dicordeg@mail.ru>
2025-12-31 18:43:20 +0300

    Added SMTP support for auth-codes.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net/smtp"
 	"os"
 )
 
@@ -11,6 +12,12 @@ type Settings struct {
 	InternalAddress string `json:"internal_address"`
 
 	MaxRegisteredUsers uint `json:"max_registered_users"`
+
+	SMTPUserName      string `json:"smtp_username"`
+	SMTPPassword      string `json:"smtp_password"`
+	SMTPHost          string `json:"smtp_host"`
+	SMTPPort          string `json:"smtp_port"`
+	SMTPSenderAddress string `json:"smtp_sender_address"`
 }
 
 func main() {
@@ -21,6 +28,12 @@ func main() {
 		InternalAddress: ":5302",
 
 		MaxRegisteredUsers: 100,
+
+		SMTPUserName:      "Admin",
+		SMTPPassword:      "Admin",
+		SMTPHost:          "localhost",
+		SMTPPort:          "587",
+		SMTPSenderAddress: "xenon_authorization_nicroservice@example.com",
 	}
 
 	content, err := os.ReadFile(SETTINGS_FILE_PATH)
@@ -40,6 +53,22 @@ func main() {
 		err = json.Unmarshal(content, &settings)
 		if err != nil {
 			log.Println("Cannot deserialize settings:", err)
+		} else {
+			log.Println("Settings successfully deserialized.")
 		}
 	}
+
+	auth := smtp.PlainAuth("", settings.SMTPUserName, settings.SMTPPassword, settings.SMTPHost)
+
+	to := "discordeg@mail.ru"
+	msg := []byte("To: " + to + "\r\n" +
+		"Subject: TEST SUBJECT\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n")
+	err = smtp.SendMail(settings.SMTPHost+":"+settings.SMTPPort, auth, settings.SMTPSenderAddress, []string{to}, msg)
+	if err != nil {
+		log.Fatal("Cannot send mail:", err)
+	}
+
+	log.Println("Message sent successfully!")
 }
